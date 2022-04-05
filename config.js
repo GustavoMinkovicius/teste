4558ce1f8957153cad05aab59abccced1abbb306
GustavoMinkovicius <87321967+GustavoMinkovicius@users.noreply.github.com>
2022-04-05 19:01:12 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyAfTK3wAsePWfKRuGbm8196ptroy1wA8NU",
+  authDomain: "app-narcao-de-historias.firebaseapp.com",
+  databaseURL: "https://app-narcao-de-historias-default-rtdb.firebaseio.com",
+  projectId: "app-narcao-de-historias",
+  storageBucket: "app-narcao-de-historias.appspot.com",
+  messagingSenderId: "915981457109",
+  appId: "1:915981457109:web:7ba2a7c41d2ca2450ff45c"
+};
+
